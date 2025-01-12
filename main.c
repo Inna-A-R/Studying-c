8be2764b7411f5e4ea7d2c661a5e55a5ceef3ee0
Inna-A-R <inna1990sweet@mail.ru>
2025-01-12 21:24:49 +0300

    second commit

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main () {
-    
-    printf ("Hello World!\n");
+
+    int num = 23;
+    float x;
+    x = 4.56;
+    char sym = 'A';
+    double y = 6.4389576;
+
+    printf ("Num is %d\n", num);
 
     return 0;
 }
